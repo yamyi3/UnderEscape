a3faceb1dcfab8f0b1c74b85113337382a013049
1chi11 <oibc36160@ojs.ac.jp>
2025-06-13 14:42:32 +0900

    タイトルの処理

--- UnderEscape/game/scene_manager/scene/title/title.h
+++ UnderEscape/game/scene_manager/scene/title/title.h
@@ -6,36 +6,38 @@
 class TitleScene :public IScene
 {
 public:
-    TitleScene();
-    ~TitleScene() = default;
+    TitleScene();                       //コンストラクタ
+    ~TitleScene() = default;            //デストラクタ      
 
-    void Initialize()override;
-    void Update()override;
-    void Draw()override;
-    void Finalize()override;
-    void Title_Update();
-    void Team_Update();
-
+    void Initialize()override;          //初期化
+    void Update()override;              //更新
+    void Draw()override;                //描画
+    void Finalize()override;            //解放
+    void Title_Draw();                  //タイトルロゴの描画
+    void Title_Update();                //タイトルロゴのフェード
+    void Team_Draw();                   //チームロゴの描画
+    void Team_Update();                 //チームロゴのフェード
 private:
-    vivid::Vector2 pos;         //背景の位置（原点）
-    vivid::Vector2 spos1;       //START[ENTER]の位置
-    vivid::Vector2 spos2;       //タイトルロゴの位置
-    vivid::Vector2 tpos1;       //チームロゴの位置
+    vivid::Vector2 back_pos;            //背景の位置（原点）
+    vivid::Vector2 start_pos1;          //START[ENTER]の位置
+    vivid::Vector2 start_pos2;          //START[ENTER]の位置
+    vivid::Vector2 title_pos;           //タイトルロゴの位置
+    vivid::Vector2 team_pos;            //チームロゴの位置
 
-    int team_time;              //チームロゴの透明、不透明になるまでのカウント
-    int title_time;             //タイトルロゴの透明、不透明になるまでのカウント
-    int hold_time;              //チームロゴの表示時間
-    int color_max_time;        //ロゴ完全に透明、不透明になるまでの時間
-    unsigned title_color;       //タイトルロゴの色の管理
-    unsigned team_color;        //チームロゴの色の管理
-    unsigned back_color;        //背景の色（固定）
-    bool team_sw;               //チームロゴの透明、不透明の切り替え
-    bool title_sw;              //タイトルロゴの透明、不透明の切り替え
+    int team_time;                      //チームロゴの透明、不透明になるまでのカウント
+    int title_time;                     //タイトルロゴの透明、不透明になるまでのカウント
+    int hold_time;                      //チームロゴの表示時間
+    int color_max_time;                 //ロゴ完全に透明、不透明になるまでの時間
+    unsigned title_color;               //タイトルロゴの色の管理
+    unsigned team_color;                //チームロゴの色の管理
+    unsigned back_color;                //背景の色（固定）
+    bool team_sw;                       //チームロゴの透明、不透明の切り替え
+    bool title_sw;                      //タイトルロゴの透明、不透明の切り替え
 
-    enum class Title_Draw       //ロゴの描画
+    enum class Opning_Draw              //ロゴの描画
     {
         Team_Logo,
         Title_Logo,
     };
-    Title_Draw Title = Title_Draw::Team_Logo;
+    Opning_Draw Title = Opning_Draw::Team_Logo;
 };
