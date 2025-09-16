190e1922011239ec38f92612d5d3c68d5266680f
klem-wolf <tm000.002mt@gmail.com>
2025-09-17 01:29:57 +0900

    エクセル改良

--- UnderEscape/game/scene_manager/scene/gamemain/stage/stage_id.h
+++ UnderEscape/game/scene_manager/scene/gamemain/stage/stage_id.h
@@ -3,9 +3,9 @@
 enum class MAP_CHIP_ID
 {
 	EMPTY,			//0
-	BLOCK,			//1
-	WALL,			//2
-	TPSTAIRS,		//3
-	START,			//4
-	GOAL,			//5
+	BLOCK,			//1ブロック
+	WALL,			//2遮蔽
+	TPSTAIRS,		//3テレポート階段
+	START,			//4スタート地点
+	GOAL,			//5ゴール地点
 };
